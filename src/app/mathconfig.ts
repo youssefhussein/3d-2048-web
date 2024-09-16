d9d10b3ab939e21efe85e12478f17cb84f14b3e3
youssef hussein <20634930+youssefhussein@users.noreply.github.com>
2024-09-16 21:12:22 +0300

    updated math config. new fullTileSize

--- src/app/mathconfig.ts
+++ src/app/mathconfig.ts
@@ -1,11 +1,13 @@
-const boardSize = 8;
-const Padding = 0.1;
-const tileSize = (boardSize - (boardSize - 1) * Padding) / boardSize;
+const boardSize = 8; // 8x8  world units is stupid
+const Padding = 0.1; //padding is 5% of 8m 
+const tileSize = 1.8; // minum tile size keep in mind padding on all sides
+const fullTileSize = tileSize + Padding; //should equate to 2
 
 const m = {
   boardSize: boardSize,
   tileSize: tileSize,
   padding: Padding,
+  fullTileSize: fullTileSize,
 };
 
 export default m;
